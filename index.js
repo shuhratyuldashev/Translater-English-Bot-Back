256630a1ecabbd1407a9c3e87f69f5810382b2dd
shuhratyuldashev <shuhratyuldoshev03@gmail.com>
2025-03-09 18:15:37 +0500

    final/prefinal v.4

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { translate } from "@vitalets/google-translate-api";
 import pLimit from 'p-limit';
+import dotenv from "dotenv";
 
 dotenv.config();
 
