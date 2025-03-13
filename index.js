ef2c069f9e5a1df79a2a9cc86f44e138277a091b
shuhrat <164529112+shuhratyuldashev@users.noreply.github.com>
2025-03-13 15:16:55 +0500

    Final v.1

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.post("/translate", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port - ${PORT}`);
 });
